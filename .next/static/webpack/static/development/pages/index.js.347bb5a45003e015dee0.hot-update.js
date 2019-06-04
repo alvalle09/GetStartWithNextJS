648e25f573ceb8e0f6d12544b13b8e07261673ed
Fred Valle <phredvalle@gmail.com>
2019-06-04 10:03:50 -0500

    initial react component for Hello Next js

---
+++ .next/static/webpack/static/development/pages/index.js.347bb5a45003e015dee0.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+var Index = function Index() {};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.347bb5a45003e015dee0.hot-update.js.map
